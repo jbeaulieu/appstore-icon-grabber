5443f2c9070773fc113a1a0c2629ce58af1b868e
Jon Beaulieu <jon@jonbeaulieu.com>
2017-10-05 14:59:35 -0500

    hello world
    Removed the cobwebs from @manbolo's 3-year-old appicon python script, corrected syntax to support modern Python 3, added ability to use local mask file

---
+++ appicon.py
@@ -0,0 +1,65 @@
+import io
+import requests
+from PIL import Image
+from slugify import slugify
+
+
+def download_icon(icon_url, title, mask):
+
+    # Download icon, apply mask.
+    icon_data = requests.get(icon_url)
+    icon = Image.open(io.BytesIO(icon_data.content))
+    icon.putalpha(mask)
+
+    # Compute and save thumbnails.
+    for size in [512]:
+        icon_resized = icon.resize((size, size), Image.ANTIALIAS)
+        icon_resized.save("icon_{0}_{1}x{1}.png".format(title, size))
+
+
+def download_app_metadata(app_id, mask):
+
+    print("download_app_json {}".format(app_id))
+
+    url = "http://itunes.apple.com/us/lookup?id={}".format(app_id)
+    r = requests.get(url)
+    if r.status_code != 200:
+        print('Error downloading {} {}'.format(url, r.status_code))
+        return
+
+    results = r.json()
+
+    print(results["results"])
+    meta = results["results"][0]
+    icon_url = meta["artworkUrl512"]
+    title = slugify(meta["trackCensoredName"])
+
+    download_icon(icon_url, title, mask)
+
+
+def download_icon_mask():
+
+    # Download the mask from Github, this way we don't
+    # have to provide mask.png and the script is self contained.
+    mask_url = ""
+    mask_data = requests.get(mask_url)
+    mask = Image.open(io.BytesIO(mask_data.content))
+    mask = mask.convert('L')
+    return mask
+
+
+def download_apps():
+
+    app_ids = [
+        "284815942"
+    ]
+
+    #mask = download_icon_mask()
+    mask_import = Image.open('mask512.png')
+    mask = mask_import.convert('L');
+    [download_app_metadata(app_id, mask) for app_id in app_ids]
+
+
+if __name__ == '__main__':
+
+    download_apps()
